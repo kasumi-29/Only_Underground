f45233e9fbbedbff9f7abb6a177d0c9e742ac62e
BuildTools <unconfigured@null.spigotmc.org>
2020-08-31 12:35:47 +0900

    プレイヤーが動くと、明るさ判定を行うように改良

--- src/main/java/kun/minecraft_plugin/tiny/only_underground/event.java
+++ src/main/java/kun/minecraft_plugin/tiny/only_underground/event.java
@@ -1,9 +1,12 @@
 package kun.minecraft_plugin.tiny.only_underground;
 
+import org.bukkit.GameMode;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerCommandPreprocessEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+
+import java.util.Objects;
 
 public class event implements Listener {
     private final Main m;
@@ -12,7 +15,12 @@ public class event implements Listener {
     }
 
     @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event){
-        System.out.println (event.getPlayer().getLocation().getBlock().getLightFromSky());
+    public void onPlayerMove(PlayerMoveEvent event){
+        Player p=event.getPlayer();
+        GameMode player_g=p.getGameMode();
+        if(player_g.equals(GameMode.CREATIVE)||player_g.equals(GameMode.SPECTATOR)){return;}
+        if(Objects.requireNonNull(event.getTo()).getBlock().getLightFromSky()!=0){
+            p.setHealth(0d);
+        }
     }
 }
